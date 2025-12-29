8960bef34c37275e191c46891d52d9a655945859
Faezeh <faezehadabi9@gmail.com>
2025-12-29 20:26:44 +0330

    using spinner

--- src/Components/Modules/TableCoin.jsx
+++ src/Components/Modules/TableCoin.jsx
@@ -1,26 +1,31 @@
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
+import { RotatingLines } from "react-loader-spinner";
 
-function TableCoin({ coins }) {
+function TableCoin({ coins, isLoading }) {
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <TableRow key={coin.id} coin={coin} />
-          ))}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow key={coin.id} coin={coin} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
@@ -50,10 +55,7 @@ const TableRow = ({
       <td>{price_change.toFixed(2)}%</td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
-        <img
-          src={price_change > 0 ? chartUp : chartDown}
-          alt={name}
-        />
+        <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
       </td>
     </tr>
   );
--- src/Components/Templates/HomePage.jsx
+++ src/Components/Templates/HomePage.jsx
@@ -4,14 +4,16 @@ import { getCoinList } from "../../Services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [isLoading , setIsLoading]=useState(true); 
   useEffect(() => {
     fetch(getCoinList())
       .then((res) => res.json())
       .then((json) => setCoins(json));
+      setIsLoading(false);
   }, []);
   return (
     <div>
-      <TableCoin coins={coins} />
+      <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
 }
