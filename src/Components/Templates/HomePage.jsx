161f256add614be2f3801df09b06b7e4e45e316b
Faezeh <faezehadabi9@gmail.com>
2025-12-29 11:13:43 +0330

    Create a service to manage the API

--- src/Components/Templates/HomePage.jsx
+++ src/Components/Templates/HomePage.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import TableCoin from "../Modules/Tablecoin";
+import { getCoinList } from "../../Services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&CG-pLBx2n6DhXSgPeXMxD12vVBj	"
-    )
+    fetch(getCoinList())
       .then((res) => res.json())
       .then((json) => setCoins(json));
   }, []);
---
+++ src/Services/cryptoApi.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY = "CG-pLBx2n6DhXSgPeXMxD12vVBj";
+const getCoinList = () => {
+    return `${BASE_URL} coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&key=${API_KEY}`;
+};
+
+export { getCoinList };
