da349eda155eb23427241b9c5ed44eff53f54a1d
Faezeh <faezehadabi9@gmail.com>
2025-12-29 19:31:56 +0330

    Extracting the tablerow component

--- src/Components/Modules/TableCoin.jsx
+++ src/Components/Modules/TableCoin.jsx
@@ -1,5 +1,5 @@
-import chartUp from "../../assets/chart-up.svg"
-import chartDown from "../../assets/chart-down.svg"
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
 
 function TableCoin({ coins }) {
   return (
@@ -16,21 +16,9 @@ function TableCoin({ coins }) {
           </tr>
         </thead>
         <tbody>
-          {coins.map(coin => 
-            <tr key={coin.id} >
-              <td>
-                <div>
-                  <img src={coin.image} alt="" />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td> 
-              <td>{coin.name}</td>
-              <td>${coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td> <img src={coin.price_change_percentage_24h>0 ? chartUp : chartDown} alt={coin.name} /> </td>
-            </tr>
-          )}
+          {coins.map((coin) => (
+            <TableRow key={coin.id} coin={coin} />
+          ))}
         </tbody>
       </table>
     </div>
@@ -38,3 +26,35 @@ function TableCoin({ coins }) {
 }
 
 export default TableCoin;
+
+const TableRow = ({
+  coin: {
+    image,
+    symbol,
+    total_volume,
+    name,
+    current_price,
+    price_change_percentage_24h: price_change,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img
+          src={price_change > 0 ? chartUp : chartDown}
+          alt={name}
+        />
+      </td>
+    </tr>
+  );
+};
