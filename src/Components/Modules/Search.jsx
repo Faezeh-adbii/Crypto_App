005b0de43dcda89fda671a50e83938dbc4fef973
Faezeh <faezehadabi9@gmail.com>
2026-01-06 13:00:23 +0330

    showing data search

--- src/Components/Modules/Search.jsx
+++ src/Components/Modules/Search.jsx
@@ -8,6 +8,7 @@ function Search({ currency, setCurrency }) {
 
   useEffect(() => {
     const controller = new AbortController();
+    setCoins([]);
     if (!text) return;
     const search = async () => {
       try {
@@ -43,6 +44,16 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
