5ddc2e13c6074675c375fc6f528d9f9253997559
Faezeh <faezehadabi9@gmail.com>
2026-01-07 11:44:30 +0330

    build modal

--- src/Components/Modules/TableCoin.jsx
+++ src/Components/Modules/TableCoin.jsx
@@ -3,7 +3,7 @@ import chartDown from "../../assets/chart-down.svg";
 import { RotatingLines } from "react-loader-spinner";
 import styles from "./TableCoin.module.css";
 
-function TableCoin({ coins, isLoading }) {
+function TableCoin({ coins, isLoading, setChart }) {
   return (
     <div className={styles.container}>
       {isLoading ? (
@@ -22,7 +22,7 @@ function TableCoin({ coins, isLoading }) {
           </thead>
           <tbody>
             {coins.map((coin) => (
-              <TableRow key={coin.id} coin={coin} />
+              <TableRow key={coin.id} coin={coin} setChart={setChart} />
             ))}
           </tbody>
         </table>
@@ -42,11 +42,15 @@ const TableRow = ({
     current_price,
     price_change_percentage_24h: price_change,
   },
+  setChart,
 }) => {
+  const showHandler=()=>{
+    setChart(true);
+  }
   return (
     <tr>
       <td>
-        <div className={styles.symbol}>
+        <div className={styles.symbol} onClick={showHandler}>
           <img src={image} alt="" />
           <span>{symbol.toUpperCase()}</span>
         </div>
--- src/Components/Templates/HomePage.jsx
+++ src/Components/Templates/HomePage.jsx
@@ -3,12 +3,14 @@ import TableCoin from "../Modules/Tablecoin";
 import { getCoinList } from "../../Services/cryptoApi";
 import Pagination from "../Modules/pagination";
 import Search from "../Modules/Search";
+import Chart from "../Modules/Chart";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("USD");
+  const [chart, setChart] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -18,14 +20,15 @@ function HomePage() {
         .then((json) => setCoins(json));
       setIsLoading(false);
     } catch (error) {
-      console.log(error); 
+      console.log(error);
     }
   }, [page, currency]);
   return (
     <div>
       <Search currency={currency} setCurrency={setCurrency} />
-      <TableCoin coins={coins} isLoading={isLoading} />
+      <TableCoin coins={coins} isLoading={isLoading} setChart={setChart} />
       <Pagination page={page} setPage={setPage} />
+      {!!chart && <Chart chart={chart} setChart={setChart} /> }
     </div>
   );
 }
