8d4b71a44c8b95dec41da0f7c01638e9f69fb801
Faezeh <faezehadabi9@gmail.com>
2026-01-08 18:21:16 +0330

    Changing the data format for charting

--- src/Components/Modules/Chart.jsx
+++ src/Components/Modules/Chart.jsx
@@ -1,12 +1,18 @@
+import { convertData } from "../../helpers/convertData";
 import styles from "./Chart.module.css";
+import { useState } from "react";
 
-function Chart({chart , setChart }) {
+function Chart({ chart, setChart }) {
+  const [type, setType] = useState("prices");
+  console.log(convertData(chart, type));
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={()=>setChart(null)} >X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}></div>
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
